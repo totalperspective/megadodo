b6c8b55e9fe9549ea5b453decb568354f9f4de2c
Neel Upadhyaya <neel.upadhyaya@eckoh.com>
2016-01-19 14:42:04 +0000

    Added core.logic

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
-(defproject totalperspective/megadodo "0.2.0"
+(defproject totalperspective/megadodo "0.3.0-SNAPSHOT"
   :description "Mustache and macros for hiccup style markup"
   :url "https://github.com/totalperspective/megadodo"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/core.match "0.3.0-alpha4"]]
+                 [org.clojure/core.match "0.3.0-alpha4"]
+                 [org.clojure/core.logic "0.8.10"]]
   :profiles {:dev {:dependencies [[midje "1.8.3"]]
                    :plugins [[lein-midje "3.1.3"]]}})
